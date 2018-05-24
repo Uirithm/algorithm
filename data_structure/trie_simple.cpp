6e7c6c3c56a87885098d512179bcf7fc2fb66133
Seongsoo Choi, PhD <blue2dea@gmail.com>
2018-05-24 03:00:33 +0000

    Update and rename trie_insert_word_simplest.cpp to trie_simple.cpp

--- data_structure/trie_simple.cpp
+++ data_structure/trie_simple.cpp
@@ -23,6 +23,43 @@ void Insert_Word(char *str)
   node->end_of_word = 1;
 }
 
+char result[11];
+int result_idx;
+void Print_All_Word(NODE* node)
+{ 
+	int i; 
+	if (node->end_of_word) printf("%s\n", result);
+ 
+	for (i = 0; i < 26; ++i) 
+	{ 
+		if (node->next[i])  
+		{   
+			result[result_idx++] = i + 'a';   
+			Print_All_Word(node->next[i]);   
+			result[--result_idx] = 0;  
+		} 
+	}
+}
+
+int Search_Word(char *str)
+{ 
+	int i; 
+	NODE* node = &root;
+ 
+	for (i = 0; str[i] != 0; ++i) 
+	{  
+		if (node->next[GETIDX(str[i])] == (NODE*)0) return 0;  
+		node = node->next[GETIDX(str[i])]; 
+	}
+ 
+	return node->end_of_word;
+}
+
+void Delete_All_Node(NODE* node)
+{ for (int i = 0; i < 26; ++i) {  if (node->next[i])  {   Delete_All_Node(node->next[i]);   free(node->next[i]);  } }}
+void init(void) { Delete_All_Node(&root);  for (int i = 0; i < 26; ++i)  root.next[i] = (NODE*)0;}
+
+/*
 void Delete_All_Node(NODE* node)
 { 
   if (node == (NODE*)0) return;  
@@ -41,6 +78,7 @@ void Init(void)
   for (int i = 0; i < 26; ++i)  
     root.next[i] = (NODE*)0;
 }
+*/
 
 int main()
 { 
